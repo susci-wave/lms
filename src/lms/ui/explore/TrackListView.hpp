51cf5ec940cb9b2b1a2ebb41e9f2994e318fa6c0
susci <susci-one@foxmail.com>
2025-10-11 20:37:34 +0800

    -

--- src/lms/ui/explore/TrackListView.hpp
+++ src/lms/ui/explore/TrackListView.hpp
@@ -40,7 +40,7 @@ namespace lms::ui
         void refreshView();
         void addSome();
         void renameTrackList();
-        void doRenameTrackList();
+        void TrackList::doRenameTrackList(const Wt::WString& name)；
 
         static constexpr std::size_t _batchSize{ 6 };
         static constexpr std::size_t _maxCount{ 8000 };
